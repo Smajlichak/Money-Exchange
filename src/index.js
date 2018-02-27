82446fbe319b02a647a5a02fe82c9ec810666aaa
Smajlichak <smajlichak@yandex.ru>
2018-02-27 23:18:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var coins = [[50, "H"], [25, "Q"], [10, "D"], [5, "N"], [1, "P"]],i, l, count, exchange = {};
+    if (currency > 10000) {
+    	exchange.error = "You are rich, my friend! We don't have so much coins for exchange";
+    }
+    else if (currency > 0){
+    	for (i = 0, l = coins.length; i < l; i++){
+    		count = 0;
+    		while (currency >= coins[i][0]){
+					count++;
+    			currency = currency - coins[i][0];
+    			exchange[coins[i][1]] = count;
+  			}
+    	}
+    }
+    return exchange;
